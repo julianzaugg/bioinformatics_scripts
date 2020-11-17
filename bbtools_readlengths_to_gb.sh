2b2fc8a51f8bccdecd9cba207f3161a8a5659386
julianzaugg <julianzaugg@gmail.com>
2020-11-17 14:29:30 +1000

    added bbtools_readlengths_to_gb.sh

---
+++ bbtools_readlengths_to_gb.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/awk -f
+
+# Takes the output from bbtools readlength.sh and calculates the gigabases
+
+cat $1 | grep "^[^#;]" | awk '{print $0, $1*$2/1000000000}' | awk '{c+=$10}END{print c}'
